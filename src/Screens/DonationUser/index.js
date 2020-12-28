3eb67fd9a205f5c8934e1a7b07267477ccc78253
yosan27 <73471678+yosan27@users.noreply.github.com>
2020-12-28 20:17:39 +0700

    merge

--- src/Screens/DonationUser/index.js
+++ src/Screens/DonationUser/index.js
@@ -1,8 +1,28 @@
 import React, { Component } from 'react'
 import { Link, withRouter } from 'react-router-dom'
 import './donation-styles.css'
+import don1 from "./donation1.png";
+import don2 from "./donation2.png";
 
 class DonationUser extends Component {
+    state = {
+        desc:
+            "Indonesia menempati peringkat 60 dari 61 negara dengan level literasi baca",
+        author: "Unesco(2019)",
+        image: don1,
+    }
+
+    handleAjax = () => {
+
+        this.setState({
+            desc:
+                "Kehadiranmu bisa memberi kesempatan semua insan dalam meraih informasi berkualitas",
+            author: " #jendeladunia",
+            image: don2,
+        })
+    }
+
+
 
     render() {
         return (
@@ -12,17 +32,15 @@ class DonationUser extends Component {
                 </div>
                 <div class="container" id="demo">
                     <div class="row justify-content-md-center">
-                        <img src="assets/images/donation1.png" className="img-fluid" alt="Responsive image"/>
+                        <img src={this.state.image} class="img-fluid" alt="Responsive image" />
                     </div>
                     <div class="row justify-content-md-center">
-                        <h4 class="col-lg-8 text-center"><i>"Indonesia menempati peringkat 60 dari 61 negara dengan
-                            level literasi baca" </i> <b> Unesco(2019)</b> </h4>
+                        <h4 class="col-lg-8 text-center"><i> &quot;{this.state.desc}&quot; </i><b>{this.state.author}</b></h4>
                     </div>
                     <br /><br />
                     <div class="row justify-content-md-center">
                         <div class="col-lg-2">
-                            <button type="button" class="btn btn-info" onclick=
-                                "loadDoc()">Kenali lebih dalam</button>
+                            <button type="button" class="btn btn-info" onClick={this.handleAjax}>Kenali lebih dalam</button>
                         </div>
                         <div class="col-lg-2">
                             <Link to="/page/donation-form">
--- src/Screens/Login/Login.js
+++ src/Screens/Login/Login.js
@@ -1,17 +1,62 @@
 import React, { Component } from "react";
 import { Link, withRouter } from "react-router-dom";
 import "./Login-style.css";
+import { FormErrors } from './FormErrors';
 
 class Login extends Component {
-constructor(props) {
-    super(props)
-
+  constructor(props) {
+    super(props);
     this.state = {
-         user : ""
+      email: '',
+      password: '',
+      formErrors: { email: '', password: '' },
+      emailValid: false,
+      passwordValid: false,
+      formValid: false,
+      user : ""
     }
-}
+  }
 
-userHandling = (event) => {
+  handleUserInput = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    this.setState({ [name]: value },
+      () => { this.validateField(name, value) });
+  }
+
+  validateField(fieldName, value) {
+    let fieldValidationErrors = this.state.formErrors;
+    let emailValid = this.state.emailValid;
+    let passwordValid = this.state.passwordValid;
+
+    switch (fieldName) {
+      case 'email':
+        emailValid = value.match(/^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i);
+        fieldValidationErrors.email = emailValid ? '' : ' is invalid';
+        break;
+      case 'password':
+        passwordValid = value.length >= 6;
+        fieldValidationErrors.password = passwordValid ? '' : ' is too short';
+        break;
+      default:
+        break;
+    }
+    this.setState({
+      formErrors: fieldValidationErrors,
+      emailValid: emailValid,
+      passwordValid: passwordValid
+    }, this.validateForm);
+  }
+
+  validateForm() {
+    this.setState({ formValid: this.state.emailValid && this.state.passwordValid });
+  }
+
+  errorClass(error) {
+    return (error.length === 0 ? '' : 'has-error');
+  }
+      
+  userHandling = (event) => {
     this.setState({
         [event.target.name] : event.target.value
     })
@@ -20,59 +65,67 @@ userHandling = (event) => {
 loginClick = () => {
     this.props.history.push(`/${this.state.user}`)
 }
-    render() {
-        return (
-            <section className="Form my-5 mx-5 card-login">
-                <div className="container">
-                    <div className="no-gutters">
-                        <div class="col-lg-5">
-                            <img src="assets/images/cover2.png" className="img-fluid img-login" alt="" />
-                        </div>
-                        <div class="col-lg-7 px-5 pt-3">
-                            <img className="img-right" src="assets/images/book.png" alt="" />
-                            <h1 className="font-weight-bold py-2 white">Login</h1>
-                            <h4 className="txt-2 white">Welcome Back, Please Login to your account</h4>
-                            <form action="">
-                                <div className="form-row">
-                                    <div className="col-lg-7">
-                                        <input type="email" placeholder="Email Address" className="form-control my-2 p-4 box email"
-                                            required name="user" value={this.state.user} onChange={this.userHandling}/>
-                                        <span id="wrong-user" className="hide">Email is not registered!</span>
-                                        <input type="password" placeholder="Password" className="form-control my-2 p-4 box password"
-                                            required />
-                                        <span id="wrong-pass" className="hide">Wrong password!</span>
-                                    </div>
-                                </div>
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" id="gridCheck1" />
-                                    <label class="form-check-label grey" for="gridCheck1">
-                                        Remember Me
-                                    </label>
-                                </div>
-                                <div class="form-row">
-                                    <div class="col-lg-3">
-                                            <button type="button" class="btn-login mt-3 loginBtn" onClick={this.loginClick}>Login</button>
-                                    </div>
-                                    <div class="col-lg-3">
-                                        <Link to="/register">
-                                            <a href="register.html"><button type="button" class="btn-sign-up mt-3 mb-2">Sign
-                                                Up</button></a>
-                                        </Link>
-                                    </div>
-                                </div>
-                                <div class="form-row">
-                                    <Link to="/forgot-password">
-                                        <a href="forgotPass.html" class="forgot mb-5 grey">Forgot Password?</a>
-                                    </Link>
-                                </div>
 
-                                <p class="txt-3 mg-0">By signing up, you agree to E-Perpus's</p>
-                                <p class="txt-3"><a class="txt-3"
-                                    href="https://web.kominfo.go.id/sites/default/files/users/4761/UU%2019%20Tahun%202016.pdf"
-                                    target="blank"><b>Terms and Conditions & Privacy Policy</b></a></p>
-                            </form>
-                        </div>
-                    </div>
+
+  render() {
+    return (
+      <section class="Form my-10 mx-5 pb-5 card-login">
+        <div class="container">
+          <div class="row no-gutters row-box">
+            <div class="col-lg-5">
+              <img src="assets/images/cover2.png" class="img-fluid img-page" alt="" />
+            </div>
+            <div class="col-lg-7 px-5 pt-3">
+              <img class="img-right img-page" src="assets/images/book.png" alt="" />
+              <h1 class="font-weight-bold py-2 bl-2">Login</h1>
+              <h4 class="txt-2 bl-2">
+                Welcome Back, Please Login to your account
+              </h4>
+
+              <form className="demoForm">
+                <div class="col-lg-7">
+                  <div className={`form-group ${this.errorClass(this.state.formErrors.email)}`}>
+                    <label htmlFor="email">Email address</label>
+                    <input type="email" required class="form-control my-2 p-4 box email" name="email"
+                      placeholder="Email"
+                      value={this.state.email}
+                      onChange={this.handleUserInput} />
+                  </div>
+                  <div className={`form-group ${this.errorClass(this.state.formErrors.password)}`}>
+                    <label htmlFor="password">Password</label>
+                    <input type="password" class="form-control my-2 p-4 box password" name="password"
+                      placeholder="Password"
+                      value={this.state.password}
+                      onChange={this.handleUserInput} />
+                  </div>
+                  <i className="wrong-user"><FormErrors formErrors={this.state.formErrors} /></i>
+                  <div class="col-lg-5">
+                    <button type="button" class="btn-login mt-3 loginBtn" disabled={!this.state.formValid} onClick={this.loginClick}>
+                      Login
+                    </button>
+                  </div>
+                  <div class="col-lg-5">
+                    <Link to="/register">
+                      <button type="button" class="btn-sign-up mt-3 mb-2">
+                        Sign Up
+                        </button>
+                    </Link>
+                  </div>
+                </div>
+              </form>
+
+              <p class="txt-3 mg-0">By signing up, you agree to E-Perpus's</p>
+              <p class="txt-3">
+                <a
+                  class="txt-3"
+                  href="https://web.kominfo.go.id/sites/default/files/users/4761/UU%2019%20Tahun%202016.pdf"
+                  target="blank"
+                >
+                  <b>Terms and Conditions & Privacy Policy</b>
+                </a>
+              </p>
+            </div>
+          </div>
         </div>
       </section>
     );
