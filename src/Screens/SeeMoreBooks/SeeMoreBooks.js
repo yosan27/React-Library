3092107c6d3606d518e54b37d943e36d5a877845
yosan27 <73471678+yosan27@users.noreply.github.com>
2021-01-14 16:50:48 +0700

    fix tulisan

--- src/Screens/SeeMoreBooks/SeeMoreBooks.js
+++ src/Screens/SeeMoreBooks/SeeMoreBooks.js
@@ -48,6 +48,14 @@ class SeeMoreBooks extends Component {
             <div className="content">
               <ul className="books">
               {this.state.data.map((d,i) => {
+                if (d.bookDetailsEntity.bookTitle.length > 16) {
+                  d.bookDetailsEntity.bookTitle =
+                    d.bookDetailsEntity.bookTitle.substring(0, 16) + "  ...";
+                }
+                if (d.authorEntity.authorName.length > 20) {
+                  d.authorEntity.authorName =
+                    d.authorEntity.authorName.substring(0, 20) + "  ...";
+                }
                 return(
                   <Link to={{pathname: `/page/detailpage/${d.bookCode}`}}>
                     <li>
